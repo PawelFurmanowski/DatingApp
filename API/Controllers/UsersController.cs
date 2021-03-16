cec59f3aa09e15c5f2f74d6cf9d1032004732320
PawelF <p.furmanowski28@gmail.com>
2021-03-16 15:41:28 +0100

    Modified UsersController to get async code

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -21,22 +23,24 @@ namespace API.Controllers
         //IEnumerable pozwala na prostą iterację po na kolekcji 
         //Zamiast IEnumerable moglibyśmy użyć List<AppUser> ale lista oferuje zbyt wiele metod a my potrzebujemy jedynie zwrócić userów
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task< ActionResult<IEnumerable<AppUser>> >GetUsers()
         {
             //ToList() potrzebuje LINQ
-            var users = _context.Users.ToList();
+            //ToListAsync() potrzebuje Microsoft.EntityFrameworkCore;
+            var users = await _context.Users.ToListAsync();
 
             return users;
         }
 
         //  api/users/3 => zwróci pojedyńczego usera
         // zwracamy pojedyńczego usera więc IEnumerable nie jest nam potrzbene (to nie jest lista)
-        //id bierzemy z 
+         
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>>GetUser(int id)
         {
             //Find() potrzebuje LINQ zwraca zmienną o określonym id
-            var user = _context.Users.Find(id);
+            //FindAsync() potrzebuje Microsoft.EntityFrameworkCore; zwraca zmienną o określonym id
+            var user = await _context.Users.FindAsync(id);
 
             return user;
         }
