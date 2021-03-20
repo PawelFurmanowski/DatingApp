baf4d0c9d65d6c584f9441ace4bccb2d140aa959
PawelF <p.furmanowski28@gmail.com>
2021-03-20 22:16:33 +0100

    Client - Using HTTPS in angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -67,7 +67,7 @@ namespace API
             // Ustalamy konkretne źródło pochodzenia dla nagłówków oraz metod
 
             //w skrócie możesz wszystkot to => AllowAnyHeader().AllowAnyMethod() TYLKO gdy pochodzi z tego =>  WithOrigins("http://localhost:4200")
-            app.UseCors( policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors( policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
